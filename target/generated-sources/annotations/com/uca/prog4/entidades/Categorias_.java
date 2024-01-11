03b20511c8e8d86c80fd392fa0bb3f4c98b1d277
Mauricio Da Re <daremauricio5@gmail.com>
2024-01-11 20:25:21 -0300

    first commit

---
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Categorias_.java
@@ -0,0 +1,17 @@
+package com.uca.prog4.entidades;
+
+import com.uca.prog4.entidades.Productos;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-16T20:18:52")
+@StaticMetamodel(Categorias.class)
+public class Categorias_ { 
+
+    public static volatile SingularAttribute<Categorias, Integer> categoria;
+    public static volatile CollectionAttribute<Categorias, Productos> productosCollection;
+    public static volatile SingularAttribute<Categorias, String> nombre;
+
+}
