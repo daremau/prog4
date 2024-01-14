f0bb62108588312d45a7f9259f81dc55b6ac4f14
mathiaShrem <m.m.shrem@gmail.com>
2024-01-14 19:13:12 -0300

    Se agrego la pagina de login

--- target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-16T20:18:52")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-13T17:16:02")
 @StaticMetamodel(Ajustes.class)
 public class Ajustes_ { 
 
--- target/generated-sources/annotations/com/uca/prog4/entidades/Proveedores_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Proveedores_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-16T20:18:52")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-13T17:16:02")
 @StaticMetamodel(Proveedores.class)
 public class Proveedores_ { 
 
