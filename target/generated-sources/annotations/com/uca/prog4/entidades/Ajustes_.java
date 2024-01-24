a97969b07b1c4188f83734a7d38c2ca3973391bf
mathiaShrem <m.m.shrem@gmail.com>
2024-01-23 22:12:37 -0300

    se modifico el edit de empleados

--- target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-21T15:38:48")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-23T21:36:42")
 @StaticMetamodel(Ajustes.class)
 public class Ajustes_ { 
 
--- target/generated-sources/annotations/com/uca/prog4/entidades/Proveedores_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Proveedores_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-21T15:38:48")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-23T21:36:42")
 @StaticMetamodel(Proveedores.class)
 public class Proveedores_ { 
 
