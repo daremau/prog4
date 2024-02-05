cbee7620811d0d47c7b774f42adfa20487b396f8
belenvalladares <belen.valladares.munoz@gmail.com>
2024-02-05 00:18:59 -0300

    Actualizacion de los lists, create, edit y views

--- target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Ajustes_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-31T18:55:39")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-04T20:08:58")
 @StaticMetamodel(Ajustes.class)
 public class Ajustes_ { 
 
--- target/generated-sources/annotations/com/uca/prog4/entidades/Categorias_.java
+++ target/generated-sources/annotations/com/uca/prog4/entidades/Categorias_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-31T18:55:39")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-02-04T20:08:58")
 @StaticMetamodel(Categorias.class)
 public class Categorias_ { 
 
